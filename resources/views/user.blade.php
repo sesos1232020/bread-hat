2d3098caeabddade7acf5244cac82293904fc6ce
sesos1232020 <sesos1232020@gmail.com>
2021-09-12 13:39:53 +0300

    first commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,35 @@
+
+
+
+ <body class="antialiased">
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    
+<table border="1" cellspacing="0" cellpadding="">
+    <tr>
+        <td>Id</td>
+        <td>name</td>
+        <td>email</td>
+        <td>date of birth</td>
+        </tr>
+        @foreach ($users as $user)
+        <tr>
+        <td>{{$user['id']}}</td>
+        <td>{{$user['name']}}</td>
+        <td>{{$user['email']}}</td>
+        <td>{{$user['date_of_birth']}}</td>
+
+        </tr>
+        @endforeach
